6142451b373d299009b631886738fd8eb924d459
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-04-06 21:06:27 +0300

    update

--- scripts/kde_status.sh
+++ scripts/kde_status.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-temp_file=/tmp/kde_status
-temp_file_traffic="${temp_file}_traffic"
-echo "start" > $temp_file
-echo "0kB/s 0kB/s" > $temp_file_traffic
+kde_status=/tmp/kde_status
+kde_status_traffic=/tmp/kde_status_traffic
+
+mkfifo $kde_status
+echo "0kB/s 0kB/s" > $kde_status_traffic
 
 while true; do
 
 output=`sar -n DEV 1 1 | awk 'FNR == 6 { printf("%.0fkB/s %.0fkB/s", $5, $6); }'`
-awk -i inplace -v output="$output" '{ print output; }' $temp_file_traffic
+awk -i inplace -v output="$output" '{ print output; }' $kde_status_traffic
 
 done &
 
@@ -18,15 +19,12 @@ date=`date +"[%A] [%B] [%d-%m-%Y] [%H:%M:%S:%2N]"`
 
 memory=`free -h | awk 'FNR == 2 { print $3 "/" $2; }'` 
 
-celsius="°C"
-cpu_temp=`cat /sys/class/thermal/thermal_zone0/temp | cut -c1-2`$celsius 
+cpu_temp=`cat /sys/class/thermal/thermal_zone0/temp | cut -c1-2`"°C" 
 
 cpu_usage=`vmstat | awk 'FNR == 3 { print 100 - $15 "%"; }'` 
 
-traffic=`cat $temp_file_traffic`
+traffic=`cat $kde_status_traffic`
 
-output="$date [$memory] [$cpu_temp $cpu_usage] [$traffic]"
-
-awk -i inplace -v output="$output" '{ print output; }' $temp_file
+echo "$date [$memory] [$cpu_temp $cpu_usage] [$traffic]" > $kde_status
 
 done
