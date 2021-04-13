4f5614ec892de35371c2d26f3d0a12a8e84fee54
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-04-14 00:00:00 +0300

    some changes, don't remember what

--- scripts/dot_bashrc.sh
+++ scripts/dot_bashrc.sh
@@ -1,296 +1,336 @@
 #!/bin/bash
 
-# If not running interactively, don't do anything
-  case $- in
-      *i*) ;;
-        *) return;;
-  esac
+# default stuff 
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-  shopt -s checkwinsize
+  # if not running interactively, don't do anything
+    case $- in
+        *i*) ;;
+          *) return;;
+    esac
 
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
-  #shopt -s globstar
+  # check the window size after each command and, if necessary,
+  # update the values of LINES and COLUMNS.
+    shopt -s checkwinsize
 
-# make less more friendly for non-text input files, see lesspipe(1) 
+  # if set, the pattern "**" used in a pathname expansion context will
+  # match all files and zero or more directories and subdirectories.
+    #shopt -s globstar
 
-  [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-  
-# set variable identifying the chroot you work in (used in the prompt below)
+  # make less more friendly for non-text input files, see lesspipe(1) 
 
-  if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-      debian_chroot=$(cat /etc/debian_chroot)
-  fi
+    [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+    
+  # set variable identifying the chroot you work in (used in the prompt below)
 
-# set a fancy prompt (non-color, unless we know we "want" color)
+    if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+        debian_chroot=$(cat /etc/debian_chroot)
+    fi
 
-  case "$TERM" in
-      xterm-color|*-256color) color_prompt=yes;;
-  esac
-  
-# uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of commands, not on the prompt
-  
-  force_color_prompt=yes
-  if [ -n "$force_color_prompt" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-      # We have color support; assume it's compliant with Ecma-48
-      # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-      # a case would tend to support setf rather than setaf.)
-      color_prompt=yes
+  # set a fancy prompt (non-color, unless we know we "want" color)
+
+    case "$TERM" in
+        xterm-color|*-256color) color_prompt=yes;;
+    esac
+    
+  # uncomment for a colored prompt, if the terminal has the capability; turned
+  # off by default to not distract the user: the focus in a terminal window
+  # should be on the output of commands, not on the prompt
+    
+    force_color_prompt=yes
+    if [ -n "$force_color_prompt" ]; then
+      if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+      else
+        color_prompt=
+      fi
+    fi
+    if [ "$color_prompt" = yes ]; then
+      PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
     else
-      color_prompt=
+      PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
     fi
-  fi
-  if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-  else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-  fi
-  unset color_prompt force_color_prompt
-  
-#If this is an xterm set the title to user@host:dir
-  
-  case "$TERM" in
-    xterm*|rxvt*)
-      PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-      ;;
-    *)
-      ;;
-  esac
-  
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc). 
-  
-  if ! shopt -oq posix; then
-    if [ -f /usr/share/bash-completion/bash_completion ]; then
-        . /usr/share/bash-completion/bash_completion
-    elif [ -f /etc/bash_completion ]; then
-        . /etc/bash_completion
+    unset color_prompt force_color_prompt
+    
+  # if this is an xterm set the title to user@host:dir
+    
+    case "$TERM" in
+      xterm*|rxvt*)
+        PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+        ;;
+      *)
+        ;;
+    esac
+    
+  # enable programmable completion features (you don't need to enable
+  # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+  # sources /etc/bash.bashrc). 
+    
+    if ! shopt -oq posix; then
+      if [ -f /usr/share/bash-completion/bash_completion ]; then
+          . /usr/share/bash-completion/bash_completion
+      elif [ -f /etc/bash_completion ]; then
+          . /etc/bash_completion
+      fi
     fi
-  fi
 
-# random variables
+# variables
 
-  export LC_ALL="en_US.UTF-8" # all locales are overwritten
-  export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01' # colored GCC warnings and errors
+  # random variables
 
-# bash history
+    export LC_ALL="en_US.UTF-8" # all locales are overwritten
+    export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01' # colored GCC warnings and errors
 
-  export HISTFILE=~/.bash_history # bash history
-  export HISTCONTROL="ignoreboth:erasedups" # don't put duplicate lines or lines starting with space in the bash history.
-  export HISTFILESIZE= # unlimited bash history (file size)
-  export HISTSIZE= # unlimited bash history (lines) 
-  shopt -s histappend # append to the history file, don't overwrite it
-  # flushes the command to the history file immediately (otherwise, this would happen only when the shell exits
-  export PROMPT_COMMAND='
-    history -a;
-    printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"
-  '
+  # bash history
+
+    export HISTFILE=~/.bash_history # bash history
+    export HISTCONTROL="ignoreboth:erasedups" # don't put duplicate lines or lines starting with space in the bash history.
+    export HISTFILESIZE= # unlimited bash history (file size)
+    export HISTSIZE= # unlimited bash history (lines) 
+    shopt -s histappend # append to the history file, don't overwrite it
+    # flushes the command to the history file immediately (otherwise, this would happen only when the shell exits
+    export PROMPT_COMMAND='
+      history -a;
+      printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"
+    '
 
 # bindings
 
   #bind -x '"\e[A": history_item' # up arrow
   #bind -x '"\e[B": history_item' # down arrow
 
-# random aliases
+# aliases 
 
-  # send a notification    
-  send_desktop_notification() {
+  # random aliases
 
-    alert_message="${1:- }"
-    alert_icon="${2:-error}"
-    notify-send --urgency=low -i "$alert_icon" "$alert_message"
+    # send a notification    
+    send_desktop_notification() {
 
-  }; export -f send_desktop_notification
+      alert_message="${1:- }"
+      alert_icon="${2:-error}"
+      notify-send --urgency=low -i "$alert_icon" "$alert_message"
 
-  # show a vertical list of files with some info
-  l() { 
+    }; export -f send_desktop_notification
 
-    ls --color=auto -ahcFg "$@"
-  
-  }; export -f l
-
-  # kde status
-  kde_resources() { 
+    # reload bashrc
+    sb() { 
+      # shellcheck source=/dev/null 
+      # it silences shellcheck warnings about non-constant source
+      source ~/.bashrc
     
-    memory=$(
-      free -h | # show memory in human readable form 
-      awk ' 
-        FNR == 2 { 
-          ram_usage=$3; total_memory=$2; 
-        };
-        FNR == 3 {
-          swap_usage=$3;
-          print ram_usage "+" swap_usage "/" total_memory; 
-        };
-      '
-    )
-    cpu_temp=$( # outputs only the first two symbols of the file
-      cut -c1-2 /sys/class/thermal/thermal_zone0/temp
-    )°C
-    cpu_usage=$(
-      vmstat |
-      awk '
-        FNR == 3 { 
-          print 100 - $15 "%"; 
-        }
-      '
-    )
-    traffic=$(
-      sar -n DEV 1 1 | 
-      awk '
-        FNR == 6 { 
-          printf("%.0fkB/s %.0fkB/s", $5, $6);
-        }
-      '
-    )
-    echo "[$traffic] [$memory] [$cpu_temp $cpu_usage]" 
-  
-  }; export -f kde_resources
+    }; export -f sb
 
-  # kde date
-  kde_date() {
+    # default compile command
+    mi() { 
 
-    date +"[%A] [%B] [%d-%m-%Y] [%H:%M:%S:%3N]"
+      make &&
+      sudo make install 
 
-  }; export -f kde_date
+    }; export -f mi
 
-  # clone a repository
-  gc() { 
+    # start vpn
+    yv() {
+      
+      echo '-hfo3-!W' | xclip -sel clip
+      echo "token is in your clipboard"
+      cd=~/Repositories/ya_vpn/
+      config=~/Repositories/ya_vpn/openvpn.conf
+      sudo openvpn --cd $cd --config $config
+      
+    }; export -f yv
 
-    git clone "$@";
+    # history item
+    history_item() {
 
-  }; export -f gc
-  
-  # default git push
-  gd() {
+      history_item=$(
+        history -w /dev/stdout | # shows history without numbers
+        tac | # reverses output, so recent entries are on top
+        dmenu -l 10 # dmenu pipe
+      )
+      history -s "$history_item"
+      echo "${PS1@P}$history_item"
+      echo "$history_item" | ${SHELL:-"/bin/sh"}
 
-    message=${1:-update}
-    day=${2:-$(date +"%d")}
-    month=${3:-$(date +"%m")}
-    year=${4:-$(date +"%Y")}
-    date="$year-$month-$day 00:00:00"
-    export GIT_AUTHOR_DATE="$date"
-    export GIT_COMMITTER_DATE="$date"
-    git add . && git commit -m "$message" && git push origin
+    }; export -f history_item
 
-  }; export -f gd
+    # start xorg-server
+    sx() { 
+      
+      startx
+      
+    }; export -f sx
 
-  # default compile command
-  mi() { 
-
-    make &&
-    sudo make install 
-
-  }; export -f mi
-
-  # start vpn
-  yv() {
+    # python
+    py() { 
+      
+      python "$@"
     
-    echo '-hfo3-!W' | xclip -sel clip
-    echo "token is in your clipboard"
-    cd=~/Repositories/ya_vpn/
-    config=~/Repositories/ya_vpn/openvpn.conf
-    sudo openvpn --cd $cd --config $config
-    
-  }; export -f yv
+    }; export -f gc
 
-  # history item
-  history_item() {
+  # git aliases
 
-    history_item=$(
-      history -w /dev/stdout | # shows history without numbers
-      tac | # reverses output, so recent entries are on top
-      dmenu -l 10 # dmenu pipe
-    )
-    history -s "$history_item"
-    echo "${PS1@P}$history_item"
-    echo "$history_item" | ${SHELL:-"/bin/sh"}
+    # clone a repository
+    gc() { 
 
-  }; export -f history_item
+      git clone "$@";
 
-  # start xorg-server
-  sx() { 
+    }; export -f gc
     
-    startx
-    
-   }; export -f sx
+    # default git push
+    gd() {
 
-  # python
-  py() { 
+      message=${1:-update}
+      day=${2:-$(date +"%d")}
+      month=${3:-$(date +"%m")}
+      year=${4:-$(date +"%Y")}
+      date="$year-$month-$day 00:00:00"
+      export GIT_AUTHOR_DATE="$date"
+      export GIT_COMMITTER_DATE="$date"
+      git add . &&
+      git commit -m "$message" &&
+      git push origin
+
+    }; export -f gd
+
+  # kde aliases
+
+    # resources
+    kde_resources() { 
+      
+      memory=$(
+        free -h | # show memory in human readable form 
+        awk ' 
+          FNR == 2 { 
+            ram_usage=$3; total_memory=$2; 
+          };
+          FNR == 3 {
+            swap_usage=$3;
+            print ram_usage "+" swap_usage "/" total_memory; 
+          };
+        '
+      )
+      cpu_temp=$( # outputs only the first two symbols of the file
+        cut -c1-2 /sys/class/thermal/thermal_zone0/temp
+      )°C
+      cpu_usage=$(
+        vmstat |
+        awk '
+          FNR == 3 { 
+            print 100 - $15 "%"; 
+          }
+        '
+      )
+      traffic=$(
+        sar -n DEV 1 1 | 
+        awk '
+          FNR == 6 { 
+            printf("%.0fkB/s %.0fkB/s", $5, $6);
+          }
+        '
+      )
+      echo "[$memory] [$cpu_temp $cpu_usage] [$traffic]" 
     
-    python "$@"
-  
-  }; export -f gc
+    }; export -f kde_resources
+
+    # date
+    kde_date() {
+
+      date +"[%A] [%B] [%d-%m-%Y] [%H:%M:%S:%3N]"
+
+    }; export -f kde_date
+
+  # ls aliases
+
+      # default
+      l() { 
+
+        ls --color --classify --size --human-readable "$@"
+      
+      }; export -f l
+
+      # hidden files
+      la() { 
+
+        l --all "$@"
+      
+      }; export -f l
+
+      # vertical
+      ll() { 
+
+        l -l --no-group "$@"
+      
+      }; export -f ll
 
 
-# pacman aliases
+  # pacman aliases
 
-  # install a package
-  pm() { 
-    
-    sudo pacman -S "$@"
-    
-  }; export -f pm
+    # install a package
+    pm() { 
+      
+      sudo pacman -S "$@"
+      
+    }; export -f pm
 
-  # system update
-  pms() { 
+    # system update
+    pms() { 
+      
+      pm -yu "$@"
+      
+    }; export -f pms
     
-    sudo pacman -Syu "$@"
-    
-  }; export -f pms
-  
-  # remove a package
-  pmr() { 
-    
-    sudo pacman -R "$@"
-    
-  }; export -f pmr
+    # remove a package
+    pmr() { 
+      
+      sudo pacman -R "$@"
+      
+    }; export -f pmr
 
-  # show files installed by a package
-  pmq() { 
-    
-    sudo pacman -Ql "$@"
-    
-   }; export -f pmq
+    # show files installed by a package
+    pmq() { 
+      
+      sudo pacman -Ql "$@"
+      
+    }; export -f pmq
 
-# logout aliases
+  # logout aliases
 
-  # log out
-  lo() { 
     # that magic command logs you out of the kde session
-    qdbus org.kde.ksmserver /KSMServer logout 0 3 3
-  
-  }; export -f lo
-  
-  # lock session
-  lol() { 
-    
-    loginctl lock-session 1
-    
-  }; export -f lol
-  
-  # reboot
-  lor() { 
-    
-    reboot
-    
-   }; export -f lor
-  
-  # shutdown immediately
-  los() { 
-    
-    shutdown now
-    
-  }; export -f los
+    lo() { 
 
-# start xserver on login
+      qdbus org.kde.ksmserver /KSMServer logout 0 3 3
+    
+    }; export -f lo
+    
+    # lock session
+    lol() { 
+      
+      loginctl lock-session 1
+      
+    }; export -f lol
+    
+    # reboot
+    lor() { 
+      
+      reboot
+      
+    }; export -f lor
+    
+    # shutdown immediately
+    los() { 
+      
+      shutdown now
+      
+    }; export -f los
 
-if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-  exec startx
-fi
+  # start xserver on login
+
+    if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+      echo "Start xorg-server? y/n"
+      read -r answer
+      if [ "$answer" != "n" ] && [ "$answer" != "N" ]; then
+        exec startx
+      fi
+    fi
--- scripts/setup_hard_links.sh
+++ scripts/setup_hard_links.sh
@@ -2,25 +2,31 @@
 
 cd ~/Repositories/dot-files/ || exit
 
-# vscode_path=~/.config/Code/User/settings.json
+#vscode_path=~/.config/Code/User/settings.json # path on windows
 vscode_path=~/.config/Code\ -\ OSS/User/settings.json
 firefox_path=~/.mozilla/firefox/zq1ebncv.default-release/chrome
 
 _ln () { 
+
   rm "$2" 2>/dev/null || true
   ln "$1" "$2"
+
 }
 _mkdir () {
+  
   rm -r "$1" 2>/dev/null || true;
   mkdir "$1"
+
 }
 _cp () {
+  
   cp -r "$1" "$2" 2>/dev/null || true;
+
 }
 
 _ln configs/vscode_settings.json "$vscode_path"
-_ln configs/bashrc ~/.bashrc
-_ln configs/xinitrc ~/.xinitrc
+_ln scripts/dot_bashrc.sh ~/.bashrc
+_ln scripts/dot_xinitrc.sh ~/.xinitrc
 
 _mkdir "$firefox_path"
 _cp firefox/img "$firefox_path"
