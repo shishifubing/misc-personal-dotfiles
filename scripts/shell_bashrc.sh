8c26016839d3806abb49d93def6be2cedfbad4bd
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-04-24 00:00:00 +0300

    some_stuff

--- scripts/shell_bashrc.sh
+++ scripts/shell_bashrc.sh
@@ -1,69 +1,73 @@
 #!/usr/bin/env bash
 
-source ~/Repositories/dot-files/scripts/shell_functions.sh
-source ~/Repositories/dot-files/scripts/shell_preexec.sh
+source ~/dot-files/scripts/shell_functions.sh
+source ~/dot-files/scripts/shell_preexec.sh
 set -o vi
 
-# default stuff 
+# if not running interactively, don't do anything
+case $- in
+*i*) ;;
+*) return ;;
+esac
 
-  # if not running interactively, don't do anything
-    case $- in
-        *i*) ;;
-          *) return;;
-    esac
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
 
-  # check the window size after each command and, if necessary,
-  # update the values of LINES and COLUMNS.
-    shopt -s checkwinsize
+# if set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
 
-  # if set, the pattern "**" used in a pathname expansion context will
-  # match all files and zero or more directories and subdirectories.
-    #shopt -s globstar
+# make less more friendly for non-text input files, see lesspipe(1)
+[[ -x /usr/bin/lesspipe ]] && eval "$(SHELL=/bin/sh lesspipe)"
 
-  # make less more friendly for non-text input files, see lesspipe(1) 
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
 
-    [[ -x /usr/bin/lesspipe ]] && eval "$(SHELL=/bin/sh lesspipe)"
-    
-  # enable programmable completion features (you don't need to enable
-  # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-  # sources /etc/bash.bashrc). 
-    
-    if ! shopt -oq posix; then
-      if [ -f /usr/share/bash-completion/bash_completion ]; then
-          . /usr/share/bash-completion/bash_completion
-      elif [ -f /etc/bash_completion ]; then
-          . /etc/bash_completion
-      fi
-    fi
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
 
 # variables
 
-  # random variables
+# random variables
 
-    export LC_ALL="en_US.UTF-8" # all locales are overwritten
-    export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01' # colored GCC warnings and errors       
-  # bash history
+export PS1=$(get_shell_prompt) # shell prompt
+export LC_ALL="en_US.UTF-8"    # all locales are overwritten
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+# colored GCC warnings and errors
+# bash history
 
-    export HISTFILE=~/.bash_history # bash history
-    export HISTCONTROL= #"ignoreboth:erasedups" # don't put duplicate lines or lines starting with space in the bash history.
-    export HISTFILESIZE= # unlimited bash history (file size)
-    export HISTSIZE= # unlimited bash history (lines) 
-    shopt -s histappend # append to the history file, don't overwrite it
-    # flushes the command to the history file immediately (otherwise, this would happen only when the shell exits
-    #export PROMPT_COMMAND='PS1=$(get_shell_prompt)' # command prompt
+export HISTFILE=~/.bash_history # bash history
+export HISTCONTROL=
+#"ignoreboth:erasedups" # don't put duplicate lines or lines starting with space in the bash history.
+export HISTFILESIZE= # unlimited bash history (file size)
+export HISTSIZE=     # unlimited bash history (lines)
+shopt -s histappend  # append to the history file, don't overwrite it
+# flushes the command to the history file immediately
+#(otherwise, this would happen only when the shell exits
+#export PROMPT_COMMAND='PS1=$(get_shell_prompt)' # command prompt
+
 # bindings
 
-  #bind -x '"\e[A": history_item' # up arrow
-  #bind -x '"\e[B": history_item' # down arrow
-  setxkbmap -option caps:escape # remaps caps lock to escape
-  #xmodmap -e "keycode 58 = Escape"
-# start stuff when a shell starts
-  
-  # start xorg on login
-  if [[ -z "${DISPLAY}" ]] && [[ "${XDG_VTNR}" -eq 1 ]]; then
-    echo "Start xorg-server? y/n"
-    read -r answer
-    if [[ "$answer" != "n" ]] && [[ "$answer" != "N" ]]; then
-      exec startx
-    fi
+#bind -x '"\e[A": history_item' # up arrow
+#bind -x '"\e[B": history_item' # down arrow
+#setxkbmap -option caps:escape # remaps caps lock to escape
+# not needed since in KDE you directly remap the key
+#xmodmap -e "keycode 58 = Escape"
+
+# start stuff
+
+# start xorg on login
+if [[ -z "${DISPLAY}" ]] && [[ "${XDG_VTNR}" -eq 1 ]]; then
+  echo "Start xorg-server?"
+  read -r answer
+  if [[ "$answer" != "n" ]] && [[ "$answer" != "N" ]]; then
+    exec startx
   fi
+fi
--- scripts/shell_functions.sh
+++ scripts/shell_functions.sh
@@ -1,183 +1,195 @@
 #!/usr/bin/env bash
 
-# random aliases
+# executed just after a command has been read and is about to be executed
+# the string that the user has typed is passed as the first argument.
+# uses bash-preexec
+preexec() {
 
-  # run formatter
-  run_formatter() {
+    set_window_title "$(get_directory)■$1"
+    # ■ is needed for tabbed to parse titles
 
-    filepath="$1"
-    filetype="${2:-$(get_file_type $1)}"
-    case $filetype in
-      "vimrc")
-        ;&
-      "py")
-        autopep8 --aggressive --aggressive "$filepath" 
-        ;;
-      *)
-        echo "$filetype"
-        ;;
-    esac
+}
+export -f preexec
 
-  }; export -f run_formatter
+# executed just before each prompt
+# equivalent to PROMPT_COMMAND, but more flexible and resilient.
+# uses bash-preexec
+precmd() {
 
-  # executed just after a command has been read and is about to be executed
-  # the string that the user has typed is passed as the first argument.
-  # uses bash-preexec
-  preexec() { 
-    
-    set_window_title "$(get_directory)■$1" # ■ is needed for tabbed to parse titles
- 
-  }; export -f preexec
+    set_window_title "$(get_directory)"
+    export PS1="$(get_shell_prompt)"
 
-  # executed just before each prompt
-  # equivalent to PROMPT_COMMAND, but more flexible and resilient.
-  # uses bash-preexec
-  precmd() {
-    
-    export PS1=$(get_shell_prompt) # command prompt
-    set_window_title "$(get_directory)" 
+}
+export -f precmd
 
-  }; export -f precmd
+# st in tabbed
+stt() {
 
-  # st in tabbed
-  stt() {
+    #[[ -z $TABBED_XID ]] || export TABBED_XID=$(tabbed -d)
+    #st -w "$TABBED_XID"
+    tabbed -r 2 st -w ''
 
-    export TABBED_XID=$(tabbed -d)
-    st -w "$TABBED_XID"
+}
+export -f stt
 
-  }; export -f stt
+# send a notification
+send_desktop_notification() {
 
-  # send a notification    
-  send_desktop_notification() {
-    
     alert_message="${1:- }"
     alert_icon="${2:-error}"
     notify-send --urgency=low -i "$alert_icon" "$alert_message"
-  
-  }; export -f send_desktop_notification
-  
-  # reload bashrc
-  sb() { 
 
-    # shellcheck source=/dev/null 
-    # it silences shellcheck warnings about non-constant source
+}
+export -f send_desktop_notification
+
+# reload bashrc
+sb() {
+
     source ~/.bashrc
-  
-  }; export -f sb
-  
-  # default compile command
-  mi() { 
 
-    make &&
-    sudo make install 
-  
-  }; export -f mi
+}
+export -f sb
 
-  # password manager script
-  passmenu() {
+# default compile command
+mi() {
+
+    make && sudo make install
+
+}
+export -f mi
+
+# password manager script
+passmenu() {
 
     shopt -s nullglob globstar
     typeit=0
     if [[ $1 == "--type" ]]; then
-      typeit=1
-      shift
+        typeit=1
+        shift
     fi
     prefix=${PASSWORD_STORE_DIR-~/.password-store}
-    password_files=( "$prefix"/**/*.gpg )
-    password_files=( "${password_files[@]#"$prefix"/}" )
-    password_files=( "${password_files[@]%.gpg}" )
+    password_files=("$prefix"/**/*.gpg)
+    password_files=("${password_files[@]#"$prefix"/}")
+    password_files=("${password_files[@]%.gpg}")
     password=$(printf '%s\n' "${password_files[@]}" | dmenu "$@")
     [[ -n $password ]] || exit
     if [[ $typeit -eq 0 ]]; then
-      pass show -c "$password" 2>/dev/null
+        pass show -c "$password" 2>/dev/null
     else
-      pass show "$password" | { IFS= read -r pass; printf %s "$pass"; } |
-        xdotool type --clearmodifiers --file -
+        pass show "$password" | {
+            IFS= read -r pass
+            printf %s "$pass"
+        } |
+            xdotool type --clearmodifiers --file -
     fi
 
-  }; export -f passmenu
-  
-  # start vpn
-  yv() {
-    
+}
+export -f passmenu
+
+# start vpn
+yv() {
+
     echo '-hfo3-!W' | xclip -sel clip
     echo "token is in your clipboard"
-    cd=~/Repositories/ya_vpn/
-    config=~/Repositories/ya_vpn/openvpn.conf
-    sudo openvpn --cd $cd --config $config
-    
-  }; export -f yv
-  
-  # history item
-  history_item() {
-    
+    directory=~/Repositories/ya_vpn/
+    config_file=~/Repositories/ya_vpn/openvpn.conf
+    sudo openvpn --cd $directory --config $config_file
+
+}
+export -f yv
+
+# history item
+history_item() {
+
     history_item=$(
-      history -w /dev/stdout | # shows history without numbers
-      tac | # reverses output, so recent entries are on top
-      dmenu -l 10 # dmenu pipe
+        history -w /dev/stdout | # shows history without numbers
+            tac |                # reverses output, so recent entries are on top
+            dmenu -l 10          # dmenu pipe
     )
     history -s "$history_item"
     echo "${PS1@P}$history_item"
     echo "$history_item" | ${SHELL:-"/bin/sh"}
-  
-  }; export -f history_item
-  
-  
-  db() {
-  
+
+}
+export -f history_item
+
+db() {
+
     if [[ -n "$DATABASE_KEY" ]]; then
-      choice=$(
-        echo "
+        choice=$(
+            echo "
           PRAGMA key = '$DATABASE_KEY';
           SELECT location, login FROM passwords;
-        "  |
-        sqlcipher ~/Repositories/dot-files/db.db |
-          awk 'NR > 3' |
-          dmenu -l 5
-      )
-      echo "
+        " |
+                sqlcipher ~/Repositories/dot-files/db.db |
+                awk 'NR > 3' |
+                dmenu -l 5
+        )
+        echo "
         PRAGMA key = '$DATABASE_KEY';
         SELECT password FROM passwords WHERE login==\"$choice\"
-      " |   
-      sqlcipher ~/Repositories/dot-files/db.db | 
-      xclip -sel clip
+      " |
+            sqlcipher ~/Repositories/dot-files/db.db |
+            xclip -sel clip
     fi
-  
-  }; export -f db
-  
-  # start xorg-server
-  sx() { 
-    
+
+}
+export -f db
+
+# start xorg-server
+sx() {
+
     startx
-    
-  }; export -f sx
-  
-  # vim
-  v() {
-  
+
+}
+export -f sx
+
+# vim
+v() {
+
     vim "$@"
-  
-  }; export -f v
-  
-  # python
-  py() { 
-    
+
+}
+export -f v
+
+tr() {
+
+    tar -xvzf "$@"
+}
+export -f tr
+# python aliases
+
+# python
+py() {
+
     python "$@"
-  
-  }; export -f gc
+
+}
+export -f py
+
+# python virtual environment
+pips() {
+
+    export ENVIRONMENT="pipenv"
+    pipenv shell
+    unset ENVIRONMENT
+
+}
+export -f pips
 
 # git aliases
-  
-  # clone a repository
-  gc() { 
-  
-    git clone "$@";
-  
-  }; export -f gc
-  
-  # default git push
-  gd() {
-  
+
+# clone a repository
+gc() {
+
+    git clone "$@"
+
+}
+export -f gc
+
+# default git push
+gd() {
+
     message=${1:-update}
     day=${2:-$(date +"%d")}
     month=${3:-$(date +"%m")}
@@ -186,214 +198,243 @@
     export GIT_AUTHOR_DATE="$date"
     export GIT_COMMITTER_DATE="$date"
     git add . &&
-    git commit -m "$message" &&
-    git push origin
-  
-  }; export -f gd
+        git commit -m "$message" &&
+        git push origin
+
+}
+export -f gd
 
 # kde aliases
-  
-  # resources
-  kde_resources() { 
-    
+
+# resources
+kde_resources() {
+
     memory=$(
-      free -h | # show memory in human readable form 
-      awk ' 
-        FNR == 2 { 
-          ram_usage=$3; total_memory=$2; 
+        free -h | # show memory in human readable form
+            awk '
+        FNR == 2 {
+          ram_usage=$3; total_memory=$2;
         };
         FNR == 3 {
           swap_usage=$3;
-          print ram_usage "+" swap_usage "/" total_memory; 
+          print ram_usage "+" swap_usage "/" total_memory;
         };
       '
     )
-    cpu_temp=$( # outputs only the first two symbols of the file
-      cut -c1-2 /sys/class/thermal/thermal_zone0/temp
+    cpu_temp=$(# outputs only the first two symbols of the file
+        cut -c1-2 /sys/class/thermal/thermal_zone0/temp
     )°C
     cpu_usage=$(
-      vmstat |
-      awk '
-        FNR == 3 { 
-          print 100 - $15 "%"; 
+        vmstat |
+            awk '
+        FNR == 3 {
+          print 100 - $15 "%";
         }
       '
     )
     traffic=$(
-      sar -n DEV 1 1 | 
-      awk '
-        FNR == 6 { 
+        sar -n DEV 1 1 |
+            awk '
+        FNR == 6 {
           printf("%.0fkB/s %.0fkB/s", $5, $6);
         }
       '
     )
-    echo "[$memory] [$cpu_temp $cpu_usage] [$traffic]" 
-  
-  }; export -f kde_resources
-  
-  # date
-  kde_date() {
-    
-    date +"[%A] [%B] [%Y:%m:%d] [%H:%M:%S:%3N]"
-  
-  }; export -f kde_date
+    echo "[$memory] [$cpu_temp $cpu_usage] [$traffic]"
+
+}
+export -f kde_resources
+
+# date
+kde_date() {
+
+    date +"[%A] [%B] [%Y-%m-%d] [%H:%M:%S:%3N]"
+
+}
+export -f kde_date
+
+# code-oss
+co() {
+
+    workspace_file=~/dot-files/configs/vscode_workspace.code-workspace
+    code-oss --reuse-window --no-sandbox --unity-launch "$workspace_file" "$1"
+
+}
+export -f co
 
 # ls aliases
-  
-  # default
-  l() { 
-  
+
+# default
+l() {
+
     ls --color --human-readable "$@"
-  
-  }; export -f l
-  
-  # hidden files
-  la() { 
-  
+
+}
+export -f l
+
+# hidden files
+la() {
+
     l --all --size "$@"
-  
-  }; export -f l
-  
-  # vertical
-  ll() { 
-  
+
+}
+export -f l
+
+# vertical
+ll() {
+
     l -l --no-group "$@"
-  
-  }; export -f ll
 
-  # cd
-  c() {
+}
+export -f ll
 
-    cd "$1" || exit
+# cd
+c() {
+    directory="${1:-/home/borinskikh}"
+    cd "$directory" || true
     l
 
-  }; export -f c
+}
+export -f c
 
 # pacman aliases
-  
-  # install a package
-  pm() { 
-    
+
+# install a package
+pm() {
+
     sudo pacman -S "$@"
-    
-  }; export -f pm
-  
-  # system update
-  pms() { 
-    
+
+}
+export -f pm
+
+# system update
+pms() {
+
     pm -yu "$@"
-    
-  }; export -f pms
-  
-  # remove a package
-  pmr() { 
-    
+
+}
+export -f pms
+
+# remove a package
+pmr() {
+
     sudo pacman -R "$@"
-    
-  }; export -f pmr
-  
-  # show files installed by a package
-  pmq() { 
-    
+
+}
+export -f pmr
+
+# show files installed by a package
+pmq() {
+
     sudo pacman -Ql "$@"
-    
-  }; export -f pmq
+
+}
+export -f pmq
 
 # logout aliases
-  
-  # that magic command logs you out of the kde session
-  lo() { 
-    
+
+# logs out of the kde session
+lo() {
+
     qdbus org.kde.ksmserver /KSMServer logout 0 3 3
-  
-  }; export -f lo
-  
-  # lock session
-  lol() { 
-    
+
+}
+export -f lo
+
+# lock session
+lol() {
+
     loginctl lock-session 1
-    
-  }; export -f lol
-  
-  # reboot
-  lor() { 
-    
+
+}
+export -f lol
+
+# reboot
+lor() {
+
     reboot
-    
-  }; export -f lor
-  
-  # shutdown immediately
-  los() { 
-    
+
+}
+export -f lor
+
+# shutdown immediately
+los() {
+
     shutdown now
-    
-  }; export -f los
+
+}
+export -f los
 
 # getters and setters
 
-  # return extension of the string provided
-  get_file_type() {
-    
+# return extension of the string provided
+get_file_type() {
+
     echo "$1" |
-    awk -F'[.]' '{
-      print $NF;
-    }'
+        awk -F'[.]' '{
+            print $NF;
+        }'
 
-  }; export -f get_file_type
+}
+export -f get_file_type
 
-  # get directory
-  get_directory() {
+# get directory
+get_directory() {
 
     pwd |
-    awk -F'[/]' '{
-     print $(NF-1) "/" $NF;
-    }'
+        awk -F'[/]' '{
+            print $(NF-1) "/" $NF;
+        }'
     # sed 's|/home/borinskikh/|~/|g'
 
-  }; export -f get_directory
+}
+export -f get_directory
 
-  # get default window title
-  get_window_title() {
-
-    directory=$(get_directory)
-    command=$(
-      history -w /dev/stdout |
-      awk 'END { print; }'
-    )
-    echo "$directory|$command"
-
-  }; export -f get_window_title
-
-  # sets window title based on the string provided
-  set_window_title() {
+# set window title
+set_window_title() {
 
     echo -ne "\033]0;$1\007"
 
-  }; export -f set_window_title
+}
+export -f set_window_title
 
-  # get current git branch
-  get_current_branch() {
+# get current git branch
+get_current_branch() {
 
-   git branch --show-current 2>/dev/null
+    git branch --show-current 2>/dev/null
 
-  }; export -f get_current_branch
+}
+export -f get_current_branch
 
-  # generates the prompt
-  get_shell_prompt() {
-    
-    white="\[\e[1;37m\]"
-    red="\[\e[1;31m\]"
-    green="\[\e[1;34m\]"
-    violet="\[\e[1;36m\]"
-    blue="\[\e[1;34m\]"
-    magenta="\[\e[1;35m\]"
+# return color code
+get_color() {
 
-    username="$white\u"
-    hostname="$violet@\h"
-    directory="$blue:\w"
-    [[ -z $(get_current_branch) ]] || git_branch="$magenta:$(get_current_branch)"
-    user_sign="$white$"
-    
-    echo "$username$hostname$directory$git_branch$user_sign "
+    color_number="${1:-37}"
+    color_type="${2:-1}"
+    echo "\[\e[${color_type};${color_number}m\]"
 
-  }; export -f get_shell_prompt
+}
+export -f get_color
+
+# generate the prompt
+get_shell_prompt() {
+
+    [[ -z "$ENVIRONMENT" ]] || environment="$(get_color 32)[$ENVIRONMENT] "
+    username="$(get_color 33)[\u] "
+    hostname="$(get_color 36)[\H] "
+    directory="$(get_color 34)[\w] "
+    [[ -z "$(get_current_branch)" ]] || git_branch="$(get_color 35)[$(get_current_branch)] "
+    user_sign="$(get_color 37)\$ "
+
+    echo "${environment}${username}${hostname}${directory}${git_branch}${user_sign}"
+
+}
+export -f get_shell_prompt
+
+# get name of the process by PID
+get_name_of_the_process() {
+
+    ps -p "$1" -o comm=
+
+}
+export -f get_name_of_the_process
