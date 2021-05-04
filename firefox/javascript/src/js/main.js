746b30567c0eacbe6740db6d369bb2b8afab19b9
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-05-05 00:00:00 +0300

    removed useless stuff

--- firefox/javascript/src/js/main.js
+++
@@ -1,9 +0,0 @@
-function openPage() {
-  browser.tabs.query({currentWindow: true, active: true})
-    .then((tabs) => {
-      let base = "https://supchat.taxi.yandex-team.ru/chatterbox-api/v1/tasks/"
-      let uri = base + tabs[0].url.split('/')[4];
-      browser.tabs.create({ url: uri });  
-  })}
-
-browser.browserAction.onClicked.addListener(openPage);
