882b79e62965957d3536ca0f7b78b9558de38e23
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-10-14 19:18:43 +0300

    added functions for docker installattion

---
+++ scripts/functions/docker.sh
@@ -0,0 +1,40 @@
+install_docker_compose() {
+
+    sudo curl -L \
+        "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+        -o /usr/local/bin/docker-compose
+
+     sudo chmod +x /usr/local/bin/docker-compose
+     sudo ln -s /usr/local/bin/docker-compose \
+         /usr/bin/docker-compose
+
+}
+
+install_docker_rhel() {
+
+    sudo yum remove docker \
+        docker-client \
+        docker-client-latest \
+        docker-common \
+        docker-latest \
+        docker-latest-logrotate \
+        docker-logrotate \
+        docker-engine
+
+    sudo yum install -y yum-utils
+
+    sudo yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+
+     sudo yum install docker-ce docker-ce-cli containerd.io
+}
+
+install_docker() {
+
+    install_docker_rhel
+    sudo systemctl enable --now docker
+    sudo usermod -aG docker "${USER}"
+    sudo install_docker_compose
+
+}
