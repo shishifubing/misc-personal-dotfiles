a5c74080828a58e1fb5794297d6829f105130d2a
shishifubing <97828377+shishifubing@users.noreply.github.com>
2023-01-16 20:13:08 +0300

    updated the setup script, added scc overview to the readme

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-host="https://hashicorp-releases.yandexcloud.net"
-github="https://github.com"
-gitversion_url="${github}/GitTools/GitVersion/releases/download"
-dotfiles_repo="${github}/shishifubing-com/misc-personal-dotfiles.git"
+export DOTFILES="${DOTFILES:-${HOME}/Dotfiles}"
+
+hashicorp_url="https://hashicorp-releases.yandexcloud.net"
+github_url="https://github.com"
+gitversion_url="${github_url}/GitTools/GitVersion/releases/download"
+dotfiles_url="${github_url}/shishifubing-com/misc-personal-dotfiles.git"
 helm_url="https://get.helm.sh"
 yc_url="https://storage.yandexcloud.net/yandexcloud-yc"
-terraform_docs_url="${github}/terraform-docs/terraform-docs/releases/download"
+terraform_docs_url="${github_url}/terraform-docs/terraform-docs/releases/download"
+k8s_url="https://dl.k8s.io/release"
 glab_url="https://gitlab.com/gitlab-org/cli/-/releases"
 
 terraform_version="1.3.7"
 packer_version="1.8.5"
 gitversion_version="5.11.1"
-kubectl_version=$(curl -Ls https://dl.k8s.io/release/stable.txt)
+kubectl_version=$(curl -Ls "${k8s_url}/stable.txt")
 helm_version="v3.10.3"
 yc_version=$(curl -Ls "${yc_url}/release/stable")
 terraform_docs_version="v0.16.0"
 glab_version="1.24.1"
-
-dotfiles_dir="${HOME}/Dotfiles"
-config_dir="${dotfiles_dir}/terraform/yandex_cloud"
 
 terraform_docs_distrib="terraform-docs-${terraform_docs_version}-linux-amd64.tar.gz"
 terraform_distrib="terraform_${terraform_version}_linux_amd64.zip"
@@ -30,10 +30,10 @@ helm_distrib="helm-${helm_version}-linux-amd64.tar.gz"
 glab_distrib="glab_${glab_version}_Linux_x86_64.tar.gz"
 
 urls=(
-    "${host}/terraform/${terraform_version}/${terraform_distrib}"
-    "${host}/packer/${packer_version}/${packer_distrib}"
+    "${hashicorp_url}/terraform/${terraform_version}/${terraform_distrib}"
+    "${hashicorp_url}/packer/${packer_version}/${packer_distrib}"
     "${gitversion_url}/${gitversion_version}/${gitversion_distrib}"
-    "https://dl.k8s.io/release/${kubectl_version}/bin/linux/amd64/kubectl"
+    "${k8s_url}/${kubectl_version}/bin/linux/amd64/kubectl"
     "${yc_url}/release/${yc_version}/linux/amd64/yc"
     "${terraform_docs_url}/${terraform_docs_version}/${terraform_docs_distrib}"
     "${helm_url}/${helm_distrib}"
@@ -57,8 +57,8 @@ current="${PWD}"
 cd "${temp}"
 
 echo "update dotfiles"
-git clone "${dotfiles_repo}" "${dotfiles_dir}" || {
-    git --git-dir "${dotfiles_dir}/.git" pull
+git clone "${dotfiles_url}" "${DOTFILES}" || {
+    git --git-dir "${DOTFILES}/.git" pull
 }
 
 echo "downloading"
@@ -78,5 +78,6 @@ cd "${current}"
 rm -rf "${temp}"
 
 echo "rest of the setup"
-mkdir -pm 700 "${HOME}/Credentials"
-"${dotfiles_dir}/scripts/setup_links.sh"
+mkdir -pm 700                                     \
+    "${HOME}/Credentials" "${HOME}/.ssh" ".gnupg"
+"${DOTFILES}/scripts/setup_links.sh"
